9378959ad4edb6ccd484ab8d148696dc7db3700b
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-01-30 02:50:52 +0200

    Create sol.cs

---
+++ Medium/235. Lowest Common Ancestor of a Binary Search Tree/sol.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true)
+        {
+            if (root.val < p.val && root.val < q.val) root = root.right;
+            else if (root.val > p.val && root.val > q.val) root = root.left;
+            else break;
+        }
+        return root;
+    }
+}
