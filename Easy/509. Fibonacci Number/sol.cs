194c164846a6b90271e869c9aa3683bb1665b171
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-02-01 08:05:46 +0200

    Create sol.cs

---
+++ Easy/509. Fibonacci Number/sol.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int Fib(int n) {
+        if (n==0) return 0;
+        else if(n==1) return 1;
+        int[] arr=new int[n];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<n;i++) arr[i]=arr[i-1]+arr[i-2];
+        return arr[n-1]+arr[n-2];
+    }
+}
