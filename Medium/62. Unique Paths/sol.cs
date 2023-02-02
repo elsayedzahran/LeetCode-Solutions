2ff49119239bff7420e75bcb6689cd6f63b3004f
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-02-02 12:00:39 +0200

    Create sol.cs

---
+++ Medium/62. Unique Paths/sol.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int UniquePaths(int m, int n) {
+        int[,] dp = new int[m+1,n+1];
+         dp[1,0]=1;
+         for(int i = 1 ; i <= m ; i++)
+         {
+            for(int j = 1 ;j <= n ; j++)
+            {
+                dp[i,j] = dp[i-1,j] + dp[i,j-1];
+            }
+        }
+        return dp[m,n];
+    }
+}
