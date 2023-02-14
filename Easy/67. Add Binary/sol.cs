e415b033ed948acbf98a302651359a451f0c371a
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-02-14 12:00:42 +0200

    Create sol.cs

---
+++ Easy/67. Add Binary/sol.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string AddBinary(string a, string b) {
+        var sum = new List<int>();
+        var carry = 0;
+        for (int i = a.Length - 1, j = b.Length - 1; i >= 0 || j >= 0 || carry > 0;)
+        {
+            var firstDigit = i >= 0 ? a[i--] - '0' : 0;
+            var secondDigit = j >= 0 ? b[j--] - '0' : 0;
+            var sumDigit = firstDigit + secondDigit + carry;
+            carry = sumDigit / 2;
+            sum.Add(sumDigit % 2);
+        }
+        sum.Reverse();
+        return String.Concat(sum);
+    }
+}
